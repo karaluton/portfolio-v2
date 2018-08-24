4857d21ed1100c27995eacd0c2ec7679741f1dff
kara <karaluton@gmail.com>
2018-08-24 16:20:16 -0500

    Nav styling and functionality

---
+++ source/components/nav/index.js
@@ -0,0 +1,33 @@
+import $ from 'wee-dom';
+import $events from 'wee-events';
+import $screen from 'wee-screen';
+import { RouteHandler } from 'wee-routes';
+
+function navToggle() {
+    $events.on('.js-toggle', 'click.toggle', () => {
+        $('.js-toggle').toggleClass('-active');
+        $('.js-overlay').toggleClass('-open');
+    });
+}
+
+export default new RouteHandler({
+    init() {
+        $screen.map([
+            {
+                max: 2,
+                callback() {
+                    navToggle();
+                }
+            },
+            {
+                min: 3,
+                callback() {
+                    if ($('.js-overlay').hasClass('-open')) {
+                        $('.js-overlay').removeClass('-open');
+                    }
+                    $events.off(false, 'toggle');
+                }
+            }
+        ]);
+    }
+});
--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,11 +1,10 @@
 import $router from 'wee-routes';
 import './bootstrap';
-import header from '../components/header';
-import scroll from '../components/nav';
+import nav from '../components/nav';
 
 $router.map([
     {
         path: '/',
-        handler: [header, scroll],
+        handler: [nav],
     },
 ]).run();
